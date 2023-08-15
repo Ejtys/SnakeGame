dd6217a877177ec9fc1173958205d195653f264d
Ejtys <jwziebapn@gmail.com>
2023-08-15 12:41:40 +0200

    Add game modes

---
+++ gameMode.py
@@ -0,0 +1,74 @@
+import pygame
+import cons
+from wall import Wall
+from snake import Snake
+from ballGenerator import BallGenerator
+from label import Label
+
+
+class GameMode:
+    def __init__(self) -> None:
+        self.is_paused = False
+        
+        #support lines
+        self.lines_surface = pygame.Surface((cons.WINDOW_WIDTH, cons.WINDOW_HEIGHT))
+        self.lines_surface.set_colorkey("green")
+        self.lines_surface.set_alpha(60)
+    
+    def draw_lines(self):
+        self.lines_surface.fill("green")
+        
+        for x in range(0, cons.WINDOW_WIDTH, cons.SQUARE_SIZE):
+            pygame.draw.line(self.lines_surface, cons.LINE_COLOR, (x, 0) , (x, cons.WINDOW_HEIGHT), 1)
+        
+        for y in range(0, cons.WINDOW_HEIGHT, cons.SQUARE_SIZE):
+            pygame.draw.line(self.lines_surface, cons.LINE_COLOR, (0, y) , (cons.WINDOW_WIDTH, y), 1)
+        
+        self.screen.blit(self.lines_surface, (0, 0))
+    
+    def event_manager(self, event):
+        pass
+    
+    def draw(self):
+        pass
+    
+    def update(self, dt):
+        pass
+    
+class SinglePlayerWithWalls(GameMode):
+    def __init__(self):
+        super().__init__()
+        self.screen = pygame.display.get_surface()
+        
+        Wall.create_boundry_wall()
+        
+        #snake
+        self.snake = Snake((5,5))
+        
+        self.ball = BallGenerator(self.snake)
+        
+        #UI
+        self.score_label = Label("Score: 0", (cons.WINDOW_WIDTH / 2, 17))
+        
+    def draw(self):
+        Wall.draw_all()
+        self.snake.draw()
+        self.ball.draw()
+        
+        self.draw_lines()
+        
+        self.score_label.draw(self.screen)
+        
+    def event_manager(self, event):
+        self.snake.event_manager(event)
+        self.play_again(event)
+        
+    def update(self, dt):
+        self.ball.update()
+        self.snake.update(dt)
+        self.score_label.update_text(f"Score: {self.snake.get_score()}")
+        
+    def play_again(self, event):
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and not self.snake.is_alive:
+            self.snake = Snake((5,5))
+            self.ball = BallGenerator(self.snake)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from snake import Snake
 from ballGenerator import BallGenerator
 from wall import Wall
 from label import Label
+from gameMode import SinglePlayerWithWalls
 
 class Game:
     def __init__(self) -> None:
@@ -15,36 +16,17 @@ class Game:
         self.clock = pygame.time.Clock()
         self.prev_time = pygame.time.get_ticks()
         
-        #support lines
-        self.lines_surface = pygame.Surface((cons.WINDOW_WIDTH, cons.WINDOW_HEIGHT))
-        self.lines_surface.set_colorkey("green")
-        self.lines_surface.set_alpha(60)
-        
-        Wall.create_boundry_wall()
-        
-        #snake
-        self.snake = Snake((5,5))
-        
-        self.ball = BallGenerator(self.snake)
-        
-        #UI
-        self.score_label = Label("Score: 0", (cons.WINDOW_WIDTH / 2, 17))
+        self.game_mode = SinglePlayerWithWalls()
         
     def quit_game(self, event):
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    def play_again(self, event):
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and not self.snake.is_alive:
-            self.snake = Snake((5,5))
-            self.ball = BallGenerator(self.snake)
-
     def event_loop(self):
         for event in pygame.event.get():
             self.quit_game(event)
-            self.snake.event_manager(event)
-            self.play_again(event)
+            self.game_mode.event_manager(event)
     
     def get_delta_time(self):
         current_time = pygame.time.get_ticks()
@@ -52,27 +34,10 @@ class Game:
         self.prev_time = current_time
         return dt
     
-    def draw_lines(self):
-        self.lines_surface.fill("green")
-        
-        for x in range(0, cons.WINDOW_WIDTH, cons.SQUARE_SIZE):
-            pygame.draw.line(self.lines_surface, cons.LINE_COLOR, (x, 0) , (x, cons.WINDOW_HEIGHT), 1)
-        
-        for y in range(0, cons.WINDOW_HEIGHT, cons.SQUARE_SIZE):
-            pygame.draw.line(self.lines_surface, cons.LINE_COLOR, (0, y) , (cons.WINDOW_WIDTH, y), 1)
-        
-        self.screen.blit(self.lines_surface, (0, 0))
-    
     def draw(self):
         self.screen.fill(cons.BACKGROUND_COLOR)
         
-        Wall.draw_all()
-        self.snake.draw()
-        self.ball.draw()
-        
-        self.draw_lines()
-        
-        self.score_label.draw(self.screen)
+        self.game_mode.draw()
               
         pygame.display.flip()
     
@@ -82,9 +47,7 @@ class Game:
             
             self.event_loop()
             
-            self.ball.update()
-            self.snake.update(dt)
-            self.score_label.update_text(f"Score: {self.snake.get_score()}")
+            self.game_mode.update(dt)
             
             self.draw()
             
